94372e45283d9ee241a2a1f705fe6f585be78a08
4739279 <ranrantanen@gmail.com>
2022-10-28 14:07:53 +0400

    v 1.01

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import numpy as np
 import scipy as sp
 import scipy.stats as st
 import seaborn as sns
-import csv
+
 
 
 def approximation(x, y, d):
     fp, residuals, rank, sv, rcond = sp.polyfit(x, y, d, full=True)
-    f = sp.poly1d(fp)  # аппроксимирующая функция
+    f = sp.poly1d(fp)
     print('Коэффициент -- a %s  ' % round(fp[0], 4))
     print('Коэффициент-- b %s  ' % round(fp[1], 4))
     print('Коэффициент -- c %s  ' % round(fp[2], 4))
     y1 = [fp[0] * x[i] ** 2 + fp[1] * x[i] + fp[2] for i in range(0, len(x))]
     so = round(sum([abs(y[i] - y1[i]) for i in range(0, len(x))]) / (len(x) * sum(y)) * 100, 4)
     print('Average quadratic deviation ' + str(so))
-    fx = sp.linspace(x[0], x[-1] + 1, len(x))  # можно установить вместо len(x) большее число для интерполяции
+    fx = sp.linspace(x[0], x[-1] + 1, len(x))
     plt.plot(fx, f(fx), linewidth=2)
 
     plt.grid(True)
@@ -33,7 +33,7 @@ data = pd.DataFrame(excel_data, columns=['x', 'y'])
 x = []
 y = []
 i = 0
-d = 3 #степень полинома
+d = 3
 
 for column in data['x']:
     x.append(float(column))
